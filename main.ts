8e6afc56677d9546a2b49229b294ac1facda4a3b
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-04-23 10:12:39 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
+    if (receivedNumber == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else if (receivedNumber == 1) {
         basic.showIcon(IconNames.Angry)
     } else {
-        basic.showIcon(IconNames.Happy)
+        basic.showIcon(IconNames.Chessboard)
     }
 })
 radio.setGroup(1)
